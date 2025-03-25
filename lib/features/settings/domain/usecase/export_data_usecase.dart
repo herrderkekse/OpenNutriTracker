0eb79edb7c761cb65efeb7a984afb9613b286a1a
ben brauch <ben.j.brauch@gmail.com>
2025-03-25 20:28:00 +0100

    removed csv dependency

--- lib/features/settings/domain/usecase/export_data_usecase.dart
+++ lib/features/settings/domain/usecase/export_data_usecase.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
-import 'package:csv/csv.dart';
 import 'package:opennutritracker/core/domain/usecase/get_intake_usecase.dart';
 import 'package:opennutritracker/core/domain/entity/intake_entity.dart';
 
@@ -12,25 +11,24 @@ class ExportDataUsecase {
   Future<String> exportFoodData(DateTime startDate, DateTime endDate) async {
     // Get all intake data between dates
     List<IntakeEntity> allIntakes = [];
-    
+
     // Iterate through each day in the date range
     for (DateTime date = startDate;
         date.isBefore(endDate.add(const Duration(days: 1)));
         date = date.add(const Duration(days: 1))) {
-      
       final breakfast = await _getIntakeUsecase.getBreakfastIntakeByDay(date);
       final lunch = await _getIntakeUsecase.getLunchIntakeByDay(date);
       final dinner = await _getIntakeUsecase.getDinnerIntakeByDay(date);
       final snacks = await _getIntakeUsecase.getSnackIntakeByDay(date);
-      
+
       allIntakes.addAll([...breakfast, ...lunch, ...dinner, ...snacks]);
     }
 
     // Create CSV data
-    List<List<dynamic>> rows = [];
-    
+    final StringBuffer csv = StringBuffer();
+
     // Add header row
-    rows.add([
+    csv.writeln([
       'Date',
       'Meal Type',
       'Food Name',
@@ -40,32 +38,40 @@ class ExportDataUsecase {
       'Carbs (g)',
       'Fats (g)',
       'Proteins (g)'
-    ]);
+    ].join(','));
 
     // Add data rows
     for (var intake in allIntakes) {
-      rows.add([
+      csv.writeln([
         intake.dateTime.toString().split(' ')[0],
         intake.type.toString().split('.').last,
-        intake.meal.name,
+        _escapeCSVField(intake.meal.name ?? ''),
         intake.amount,
-        intake.unit,
+        _escapeCSVField(intake.unit),
         intake.totalKcal,
         intake.totalCarbsGram,
         intake.totalFatsGram,
         intake.totalProteinsGram
-      ]);
+      ].join(','));
     }
-
-    // Convert to CSV
-    String csv = const ListToCsvConverter().convert(rows);
 
     // Save to file
     final directory = await getApplicationDocumentsDirectory();
     final fileName = 'food_diary_${DateTime.now().millisecondsSinceEpoch}.csv';
     final file = File('${directory.path}/$fileName');
-    await file.writeAsString(csv);
+    await file.writeAsString(csv.toString());
 
     return file.path;
+  }
+
+  String _escapeCSVField(String field) {
+    // If the field contains commas, quotes, or newlines, wrap it in quotes
+    if (field.contains(RegExp(r'[,"\n]'))) {
+      // Replace any quotes with double quotes (CSV standard)
+      field = field.replaceAll('"', '""');
+      // Wrap the field in quotes
+      return '"$field"';
+    }
+    return field;
   }
 }
